a490872a59f8980873f5a1c148c180a88b8b850e
andresorbegozo <43919133+andresorbegozo@users.noreply.github.com>
2024-11-25 13:23:14 -0500

    Create config_n2.js

---
+++ config_n2.js
@@ -0,0 +1,15 @@
+// config_n2.js
+export const CONFIG = {
+    SHEET_URL: 'https://docs.google.com/spreadsheets/d/1CTuHtOBMAQ020hR4H5aCyKVMYvATzWLkU0i144YXbqs/export?format=csv',
+    LOCAL_STORAGE_KEY: 'contentData',
+    SIMILARITY_THRESHOLD: 0.8,
+    REFRESH_INTERVAL: 90, // days
+    KEYWORDS: ['marketing', 'product', 'testimonial', 'tutorial']
+};
+
+export const PLATFORMS = {
+    DRIVE: 'drive',
+    INSTAGRAM: 'instagram',
+    TIKTOK: 'tiktok',
+    FACEBOOK: 'facebook'
+};
